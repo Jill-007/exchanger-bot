18c6d9e92a7f459082e52f95ff16234d33837758
jill007@gmail.com <jill007@gmail.com>
2023-07-27 09:33:43 +0000

    Edited files:     /src/requireAll.sc     /src/scripts/money.js

--- src/requireAll.sc
+++ src/requireAll.sc
@@ -1,4 +1,4 @@
-#require: scripts/requireScripts.sc
+require: scripts/requireScripts.sc
 require: patterns/allDicts.sc
 require: COMMON/requireCommon.sc
 require: Undef/Undef.sc
--- src/scripts/money.js
+++ src/scripts/money.js
@@ -1,17 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var moneyjs = document.createElement('script');
-  moneyjs.src = 'https://openexchangerates.github.io/money.js/money.min.js';
-  document.body.appendChild(moneyjs);
-  moneyjs.addEventListener('load', function() {
-    fetch('https://www.cbr-xml-daily.ru/latest.js')
-      .then(response => response.json())
-      .then(function(data) {
-        fx.rates = data.rates;
-        fx.base = data.base;
-    });
-  });
-});
-
 var USD_value = Null
 var EUR_value = Null
 
