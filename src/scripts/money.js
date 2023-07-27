313cb74fea8e5eb8574c73fb7176744b414acf75
jill007@gmail.com <jill007@gmail.com>
2023-07-27 10:36:59 +0000

    Edited files:     /src/scripts/money.js

--- src/scripts/money.js
+++ src/scripts/money.js
@@ -1,12 +1,21 @@
 var USD_value = null
 var EUR_value = null
 
-function httpGetAsync(theUrl, callback)
+function httpGetAsyncUSD(theUrl, callback)
 {
     log("trying to get http")
     var response = $http.get("https://www.cbr-xml-daily.ru/daily_utf8.xml");
     if (response.isOk) {
-        callback(USD)
+        var ls = response.data.ValCurs.Valute;
+        for (var i=0; i<ls.length; i++){
+            if (ls[i]==undefined) {
+                continue
+            }
+            if (ls[i].ID == "R01235") {
+                USD_value = ls[i].Value;
+            }
+            log(ls[i].Value) 
+        }
         // $temp.degree = response.data.current.temperature;
     }
     log("got http")
